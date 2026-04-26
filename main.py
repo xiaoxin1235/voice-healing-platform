17ca69e0fa38b2ba6a1c34a3c366f3716596bfb9
xiaoxin1235 <weixin7212156@qq.com>
2026-04-26 15:22:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -653,5 +653,5 @@ def upload():
     except Exception as e:
         return jsonify({"error": str(e)})
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=False)
+# if __name__ == '__main__':
+#    app.run(host='127.0.0.1', port=5000, debug=False)
